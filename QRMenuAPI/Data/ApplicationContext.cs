40851930690bf0e8dbc592f024030c91d9a39fb8
Tunaberkyrky <161122955+Tunaberkyrlky@users.noreply.github.com>
2024-03-08 15:56:44 +0300

    QRMenuAPI V1

---
+++ QRMenuAPI/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using QRMenuAPI.Models;
+using QRMenuQPI.Models;
+
+namespace QRMenuAPI.Data
+{
+	public class ApplicationContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+		{
+		}
+		public DbSet<Company>? Companies { get; set; }
+        public DbSet<State>? States { get; set; }
+    }
+}
+
