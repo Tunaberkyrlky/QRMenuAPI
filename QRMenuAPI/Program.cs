6ef8c306be9d25b58d5817b56acfe41dd6694dda
Tunaberkyrky <161122955+Tunaberkyrlky@users.noreply.github.com>
2024-03-13 15:00:03 +0300

    Program.cs Seed data and role assignment were made during the installation.

--- QRMenuAPI/Program.cs
+++ QRMenuAPI/Program.cs
@@ -10,6 +10,10 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        State state;
+        IdentityRole identityRole;
+        ApplicationUser applicationUser;
+
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
@@ -42,8 +46,52 @@ public class Program
 
 
         app.MapControllers();
+        {
+            ApplicationContext? context = app.Services.CreateScope().ServiceProvider.GetService<ApplicationContext>();
+            if (context != null)
+            {
+                context.Database.Migrate();
+                if (context.States.Count() == 0)
+                {
+                    state = new State();
+                    state.Id = 0;
+                    state.Name = "Deleted";
+                    context.States.Add(state);
+                    state = new State();
+                    state.Id = 1;
+                    state.Name = "Active";
+                    context.States.Add(state);
+                    state = new State();
+                    state.Id = 2;
+                    state.Name = "Passive";
+                    context.States.Add(state);
+                }
+                context.SaveChanges();
+                RoleManager<IdentityRole>? roleManager = app.Services.CreateScope().ServiceProvider.GetService<RoleManager<IdentityRole>>();
+                if (roleManager != null)
+                {
+                    if (roleManager.Roles.Count() == 0)
+                    {
+                        identityRole = new IdentityRole("Administrator");
+                        roleManager.CreateAsync(identityRole).Wait();
+                        identityRole = new IdentityRole("CompanyAdministrator");
+                        roleManager.CreateAsync(identityRole).Wait();
+                    }
+                }
+                UserManager<ApplicationUser>? userManager = app.Services.CreateScope().ServiceProvider.GetService<UserManager<ApplicationUser>>();
+                if (userManager != null)
+                {
+                    if (userManager.Users.Count() == 0)
+                    {
+                        applicationUser = new ApplicationUser();
+                        applicationUser.UserName = "Administrator";
+                        userManager.CreateAsync(applicationUser, "Admin123!").Wait();
+                        userManager.AddToRoleAsync(applicationUser, "Administrator").Wait();
+                    }
+                }
+            }
+        }
 
-        
         app.Run();
     }
 }
